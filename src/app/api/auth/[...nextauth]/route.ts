0b8f7c21fa9297bd298a7aa8bf52b3ddc6bb2d25
utkarshh1506 <utksin9621@gmail.com>
2025-08-22 17:01:24 +0530

    CREATE: Google IDs added

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,23 @@
+import { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth";
+import GoogleProvider from 'next-auth/providers/google'
+import pool from "@/lib/db";
+
+
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID!
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET!
+
+const authOption : NextAuthOptions = {
+    session:{
+        strategy:'jwt'
+    },
+    providers:[
+        GoogleProvider({
+            clientId:GOOGLE_CLIENT_ID,
+            clientSecret: GOOGLE_CLIENT_SECRET
+        })
+    ],
+    callbacks:{
+        
+    }
+}
