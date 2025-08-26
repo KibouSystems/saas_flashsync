9059859afd2f3977caff24ee22c69f4721e4ee93
utkarshh1506 <utksin9621@gmail.com>
2025-08-26 18:09:08 +0530

    INIT: JEST initialize

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jsdom',
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+};
+
+export default createJestConfig(customJestConfig);
